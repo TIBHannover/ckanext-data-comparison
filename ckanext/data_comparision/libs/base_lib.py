1b3870e572ced42159a95e104333468bc66c3220
pooya.oa <pooya.oladazimi@tib.eu>
2021-11-26 14:55:12 +0100

    cleaning

--- ckanext/data_comparision/libs/base_lib.py
+++ ckanext/data_comparision/libs/base_lib.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
 import ckan.plugins.toolkit as toolkit
+import ckan.lib.helpers as h
 from ckan.model import Package
+import pandas as pd
 
 
 class Helper():
@@ -27,4 +29,21 @@ class Helper():
             if dt.state == 'active' and dt.type == 'dataset' and Helper.check_access_view_package(dt.id):
                 result.append(dt)
         return result
+    
+    def is_csv_or_xlsx(resource):
+        if resource.format in ['CSV', 'XLSX']:
+            return True
+        if  '.csv' in  resource.name or '.xlsx' in  resource.name:
+            return True 
+        return False
+
+    
+    def get_file(package_id, resource_id):
+        link = h.url_for('dataset.resource_download', id=str(package_id), resource_id=resource_id  ,  _external=True)
+        df = pd.read_csv(link, index_col=0)
+        # print(df.columns)
+        # print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        return list(df.columns)
+    
+
             
--- ckanext/data_comparision/plugin.py
+++ ckanext/data_comparision/plugin.py
@@ -2,11 +2,13 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from flask import Blueprint
 from ckanext.data_comparision.controllers.base import BaseController
+from ckanext.data_comparision.libs.base_lib import Helper
 
 
 class DataComparisionPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -29,3 +31,10 @@ class DataComparisionPlugin(plugins.SingletonPlugin):
         
 
         return blueprint
+    
+    #ITemplateHelpers
+
+    def get_helpers(self):
+        return {'is_csv_xlsx': Helper.is_csv_or_xlsx,
+            'get_columns': Helper.get_file
+        }
