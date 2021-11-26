9487598a5c987f53ae61ff2b28cff58c3eaaf2a7
pooya.oa <pooya.oladazimi@tib.eu>
2021-11-26 10:36:58 +0100

    added base view

--- ckanext/data_comparision/controllers/base.py
+++ ckanext/data_comparision/controllers/base.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 
+from flask import render_template
+import ckan.plugins.toolkit as toolkit
 
 class BaseController():
 
     def base_view():
 
 
-        return '0'
+        return render_template('base_index.html')
