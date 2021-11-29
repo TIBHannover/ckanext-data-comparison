d77e18aaf382a1a3589955f77d0f8700b2606a38
pooya.oa <pooya.oladazimi@tib.eu>
2021-11-29 16:34:43 +0100

    added xlsx processor

--- ckanext/data_comparision/libs/base_lib.py
+++ ckanext/data_comparision/libs/base_lib.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 import ckan.lib.helpers as h
 from ckan.model import Package
 import clevercsv
+import pandas as pd
 
 
 RESOURCE_DIR = toolkit.config['ckan.storage_path'] + '/resources/'
@@ -49,14 +50,28 @@ class Helper():
 
     
     def get_columns(package_id, resource_id, file_type):
+        file_path = RESOURCE_DIR + resource_id[0:3] + '/' + resource_id[3:6] + '/' + resource_id[6:]
         if file_type == 'csv':
             try:
-                file_path = RESOURCE_DIR + resource_id[0:3] + '/' + resource_id[3:6] + '/' + resource_id[6:]
                 df = clevercsv.read_dataframe(file_path)
                 return list(df.columns)
 
             except:
                 return ['Error']
+
+        if file_type == 'xlsx':
+            data = {}
+            try:
+                data_sheets = pd.read_excel(file_path, sheet_name=None, header=None)
+                for sheet in data_sheets.keys():
+                    temp_df = data_sheets[sheet].dropna(how='all').dropna(how='all', axis=1)
+                    headers = temp_df.iloc[0]
+                    final_data_df  = pd.DataFrame(temp_df.values[1:], columns=headers)
+                    data[sheet] = list(final_data_df.columns)
+                return data
+
+            except:
+                return {'Error': []}
     
 
             
