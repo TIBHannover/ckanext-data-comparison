545614c1c10cec6324b1a5232e5eb04c2be66726
pooya.oa <pooya.oladazimi@tib.eu>
2021-12-01 11:30:38 +0100

    added mouse click

--- ckanext/data_comparision/public/statics/page.js
+++ ckanext/data_comparision/public/statics/page.js
@@ -2,13 +2,43 @@ $(document).ready(function(){
     $('.dcom-table-cell').mouseover(function(){
         let id = $(this).attr('name');
         id = id[id.length - 1];
-        $('.dcom-column-' + id).css('background-color', 'green');
+        let checkbox = $('#col-checkbox-' + id);
+        if ($(checkbox).prop('checked') === false){
+            $('.dcom-column-' + id).css('background-color', 'yellow');        
+        }
 
     });
 
     $('.dcom-table-cell').mouseout(function(){
         let id = $(this).attr('name');
         id = id[id.length - 1];
-        $('.dcom-column-' + id).css('background-color', '');
+        let checkbox = $('#col-checkbox-' + id);
+        if ($(checkbox).prop('checked') === false){
+            $('.dcom-column-' + id).css('background-color', '');       
+        }
     });
+
+    $('.dcom-table-cell').click(function(){
+        let id = $(this).attr('name');
+        id = id[id.length - 1];
+        let checkbox = $('#col-checkbox-' + id);
+        $(checkbox).click();
+        if ($(checkbox).prop('checked') === true){
+            $('.dcom-column-' + id).css('background-color', 'green');        
+        }
+        else{
+            $('.dcom-column-' + id).css('background-color', '');
+        }
+
+
+
+
+
+        
+
+    });
+
+
+
+
 });
