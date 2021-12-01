066f0eefbcea49fc778a6efdb20d03ff514a03a9
pooya.oa <pooya.oladazimi@tib.eu>
2021-12-01 16:08:46 +0100

    added import funcion on client side

---
+++ ckanext/data_comparision/public/statics/browes.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#import-btn').click(function(){
+        send_columns_data();
+        
+    });
+});
+
+
+/**
+ * Post resources name data to backend for importing
+ */
+ function send_columns_data(){
+    let formdata = new FormData();
+    let checkboxes = $('.resource-box');
+    let checked_ones = [];
+    for (let i=0; i < checkboxes.length; i++){
+        if ($(checkboxes[i]).prop('checked') === true){
+            checked_ones.push($(checkboxes[i]).val());
+        }
+    }
+    for (let i = 0; i < checked_ones.length; i++) {
+        formdata.append('resources[]', checked_ones[i]);
+    }
+    let dest_url = $('#import_url').val();
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == XMLHttpRequest.DONE && req.status === 200) {       
+               console.info(req.responseText)                       
+        }
+    }
+    req.open("POST", dest_url);
+    req.send(formdata);
+}
