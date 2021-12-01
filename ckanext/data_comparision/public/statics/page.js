9de6001e2d587aacd417b1b438ca6890335f56ad
pooya.oa <pooya.oladazimi@tib.eu>
2021-12-01 11:05:30 +0100

    added mouse over for column

--- ckanext/data_comparision/public/statics/page.js
+++ ckanext/data_comparision/public/statics/page.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    
+    $('.dcom-table-cell').mouseover(function(){
+        let id = $(this).attr('name');
+        id = id[id.length - 1];
+        $('.dcom-column-' + id).css('background-color', 'green');
+
+    });
+
+    $('.dcom-table-cell').mouseout(function(){
+        let id = $(this).attr('name');
+        id = id[id.length - 1];
+        $('.dcom-column-' + id).css('background-color', '');
+    });
 });
