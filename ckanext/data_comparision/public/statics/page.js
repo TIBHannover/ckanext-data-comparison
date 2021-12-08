479cba590e1f9eb991431d45c1dd775b11548f2f
pooya.oa <pooya.oladazimi@tib.eu>
2021-12-08 09:58:09 +0100

    refactor code, separate the table js

--- ckanext/data_comparision/public/statics/page.js
+++ ckanext/data_comparision/public/statics/page.js
@@ -1,74 +1,6 @@
 $(document).ready(function(){
 
     /**
-     * Mouse over a column in a data table
-     */
-     $('body').on('mouseover', '.dcom-table-cell', function() {
-        let id = $(this).attr('name');
-        let checkbox = $('#' + id);
-        if ($(checkbox).prop('checked') === false){
-            $('.dcom-column-' + id).css('background-color', 'yellow');        
-        }
-    });
-
-
-    /**
-     * Mouse out a column in a data table
-     */
-     $('body').on('mouseout', '.dcom-table-cell', function() {
-        let id = $(this).attr('name');
-        let checkbox = $('#' + id);
-        if ($(checkbox).prop('checked') === false){
-            $('.dcom-column-' + id).css('background-color', '');       
-        }
-    });
-    
-
-    /**
-     * Click on a column in a data table. It click on the column checkbox
-     */
-     $('body').on('click', '.dcom-table-cell', function() {
-        let id = $(this).attr('name');
-        let checkbox = $('#' + id);
-        $(checkbox).prop('checked', !$(checkbox).is(":checked"));
-        if ($(checkbox).prop('checked') === true){
-            $('.dcom-column-' + id).css('background-color', 'green');        
-        }
-        else{
-            $('.dcom-column-' + id).css('background-color', '');
-        }
-    });
-
-    /**
-     * Click next page pagination
-     */
-     $('body').on('click', '.next-btn', function() {
-        let id_elem = $(this).attr('id');
-        let id = id_elem.split('page-next-')[1]
-        let page = $('#page-number-' + id).val();
-        let max_page = $('#page-number-max-' + id).val();
-        if(parseInt(page) < parseInt(max_page)){
-            $('#page-number-' + id).val(parseInt(page) + 1);
-            get_new_page(id, parseInt(page) + 1);
-        }
-
-    });
-
-    /**
-     * Click prev page pagination
-     */
-     $('body').on('click', '.prev-btn', function() {
-        let id_elem = $(this).attr('id');
-        let id = id_elem.split('page-prev-')[1]
-        let page = $('#page-number-' + id).val();
-        if (parseInt(page) > 1){
-            $('#page-number-' + id).val(parseInt(page) - 1);
-            get_new_page(id, parseInt(page) - 1);
-        }        
-    });
-
-
-    /**
      * Click on process data button
      */
     $('#process_btn').click(function(){
@@ -100,28 +32,6 @@ function send_columns_data(){
             let data=req.responseText;
             let jsonResponse = JSON.parse(data);   
             console.info(jsonResponse);                        
-        }
-    }
-    req.open("POST", dest_url);
-    req.send(formdata);
-}
-
-
-/**
- * Get new page on pagination
- */
- function get_new_page(resourceId, page){
-    let formdata = new FormData();
-    formdata.set('page', page);
-    formdata.set('resourceId', resourceId);
-    let dest_url = $('#new_page_url').val();
-    let req = new XMLHttpRequest();
-    req.onreadystatechange = function() {
-        if (req.readyState == XMLHttpRequest.DONE && req.status === 200) {       
-            data = JSON.parse(req.responseText);
-            let tableDiv = $('#data-table-' + resourceId).parent();
-            $('#data-table-' + resourceId).remove();
-            $(tableDiv).append(data['table']);
         }
     }
     req.open("POST", dest_url);
