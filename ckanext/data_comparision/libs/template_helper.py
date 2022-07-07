b1cce2cc8a3fe14871ce80122ad7bcc3d3029590
pooya.oa <pooya.oladazimi@tib.eu>
2022-07-07 12:33:01 +0200

    check if sfb_layout plugin enabled

--- ckanext/data_comparision/libs/template_helper.py
+++ ckanext/data_comparision/libs/template_helper.py
@@ -222,6 +222,14 @@ class TemplateHelper():
                     return ''
 
         return ''
+    
+
+    @staticmethod
+    def check_plugin_enabled(plugin_name):
+        plugins = toolkit.config.get("ckan.plugins")
+        if plugin_name in plugins:
+            return True
+        return False
 
     
     
