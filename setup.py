8ebd35c97854be384c7e4e4e56e5f7bff6161c41
pooya.oa <pooya.oladazimi@tib.eu>
2022-10-06 13:05:49 +0200

    update readme

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points='''
         [ckan.plugins]
-        data_comparision=ckanext.data_comparision.plugin:DataComparisionPlugin
+        =ckanext.data_comparision.plugin:DataComparisionPlugin
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
